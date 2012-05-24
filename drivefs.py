9b60b41d7aa36cae45d68cbd32d1f480f414c28f
Johan Forberg <johan@forberg.se>
2012-05-24 10:09:56 +0200

    readdir() and getattr() now work properly.

--- drivefs.py
+++ drivefs.py
@@ -25,8 +25,8 @@ CODING     = 'utf-8'
 class GDDir:
     def __init__(self, entry=None, dirs=[], files=[]):
         # entry == None means I am the root dir.
-#        self.name  = entry.title.text if entry else '/'
-        self.name = '/'
+        name  = entry.title.text if entry else '/'
+        self.name = name.decode(CODING)
         self.files = files
         self.dirs  = dirs
         self.stat = {
@@ -46,10 +46,10 @@ class GDDir:
         self.id = entry.resourceId.text if entry else '__root__'
 
     def child(self, name):
-        for d in dirs:
+        for d in self.dirs:
             if d.name == name:
                 return d
-        for f in files:
+        for f in self.files:
             if f.name == name:
                 return f
         # No child was found
@@ -62,7 +62,7 @@ class GDDir:
 
 class GDFile:
     def __init__(self, entry):
-        self.name = entry.title.text
+        self.name  = entry.title.text.decode(CODING)
         self.stat = {
             'st_ctime': gdtime_to_ctime(
                             entry.published.text  if entry.published  else 0),
@@ -93,24 +93,27 @@ class DriveFS(Operations):
         self.client = drive_connect(email, password)
         self.email = email
         self.root = None
-        self.refresh() # set self.root
+
+        self.refresh() # Set self.root
 
     def __del__(self):
         # Destroy drive connection
         pass
 
-    def open(self, path):
-        pl = posixpath.split(path)
-        if len(path) < 1 or path[0] != '/':
-            raise DriveFSError('Invalid path: %s' % path)
+    def gdopen(self, path):
+        pl = full_split(path)
+        if not pl or pl.pop() != '/': # Removes /
+            raise DriveFSError('Path was not absolute: %s' % path)
             return None
         f = self.root 
         try:
             while pl:
-                f = f.pl[0]
+                f = f.child(pl.pop())
         except KeyError:
             raise FuseOSError(errno.ENOENT)
             return None
+        else:
+            return f
 
     def refresh(self):
 #        q = gdocs.DocumentQuery(params={'showfolders': 'true'})
@@ -134,38 +137,16 @@ class DriveFS(Operations):
 
     def readdir(self, path, fh=None):
         if MY_DEBUG:
-            print 'readdir(%s)' % path
-        feed = self.client.GetDocumentListFeed(path_to_uri(path))
-        return ['.', '..'] + \
-            [entry.title.text.decode(CODING) for entry in feed.entry]
+            print 'readdir(%s)' % path.encode(CODING)
+        r = self.gdopen(path)
+        return ['.', '..'] + [f.name for f in r.dirs + r.files]
 
     def getattr(self, path, fh=None):
         """Build and return a stat(2)-like dict of attributes."""
         if MY_DEBUG:
-            print 'getattr(%s)' % path
-        # Default values
-        st = {'st_ctime': 0, 'st_mtime': 0, 'st_atime': 0,
-              'st_uid': self.my_uid, 'st_gid': self.my_gid,
-              'st_mode': DEFMODE, 'st_nlink': 1, 'st_size': 0}
-        if path == '/':
-            st['st_mode'] &= ~stat.S_IFREG # Is not regular
-            st['st_mode'] |=  stat.S_IFDIR # Is directory
-            return st
-        else:
-            uri = path_to_uri(path)
-            feed = None
-            if uri in self.cached_feeds:
-                feed = self.cached_feeds[uri]
-            else:
-                feed = self.client.Query(uri)
-                self.cached_feeds[uri] = feed
-            if not feed.entry:
-                raise FuseOSError(errno.ENOENT)
-            elif len(feed.entry) != 1:
-                raise Exception('Non-unique filename!')
-            f = feed.entry[0]
-            st['st_size']  = f.get_filesize()
-            return st
+            print 'getattr(%s)' % path.encode(CODING)
+        f = self.gdopen(path)
+        return f.stat
 
 def gdtime_to_ctime(timestr):
     # Note: milliseconds are stripped away.
@@ -184,12 +165,24 @@ def drive_connect(username, password):
     client.ClientLogin(username, password)
     return client
 
-def full_split(path):
-    (head, tail) = posixpath.split(path)
-    if not tail:
-        return [head,]
-    else:
-        return full_split(head).extend(tail) # Recursive
+def full_split(head):
+    """Split a path fully into components and return a reversed list."""
+    l = []
+    while True:
+        (head, tail) = posixpath.split(head)
+        if tail and head:
+            l.append(tail)
+            continue
+        elif head: # Happens for absolute paths
+            l.append(head)
+            break
+        elif tail: # Happens for non-absolute paths
+            l.append(tail)
+            break
+        else:      # Should not happen.
+            break
+    return l # Will be reversed!
+
 """
 def path_to_uri(path):
     if len(path) < 1 or path[0] != '/':
